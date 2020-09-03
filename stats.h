14e0e0c4991771edcb60ee1ccf815d8a9a5ac7dd
Balaji6701 <balabalaji6701@gmail.com>
2020-09-03 22:18:20 +0530

    Final code with documentation for assesment 1

--- stats.h
+++ stats.h
@@ -33,7 +33,23 @@
  * @return <returns the value 0>
  */
 int printstat(unsigned char *a,int val){
-     
+     printf("unsorted array \n");
+    for(int i=0;i<val;i++){ 
+        long int b= a[i];
+        printf("%ld\n",b);
+    }
+    printf("\n");
+    int s=mean(a,val);
+    printf("mean:%d\n",s);
+    int median=med(a,val);
+    printf("median:%d\n",median);
+    int maximum=max(a,val);
+    printf("maximum:%d\n",maximum);
+    int minimum=min(a,val,maximum);
+    printf("minimum:%d\n",minimum);
+    printf("sorted array \n");
+    sort(a,val);
+	return(0);
 }
 /**
  * @brief <function that calculates mean>
@@ -46,7 +62,13 @@ int printstat(unsigned char *a,int val){
  * @return <returns the variable s,which holds the mean>
  */
 int mean(unsigned char *a,int val){
-    
+    int s=0;
+    for(int i=0;i<val;i++){ 
+        long int b= a[i];
+        s=s+b;
+    }
+    s=s/val;
+    return s;
 }
 /**
  * @brief <function that calculates median>
@@ -59,7 +81,14 @@ int mean(unsigned char *a,int val){
  * @return <returns the variable median,which holds the median>
  */
 int med(unsigned char *a,int val){
-    
+    int median=0;
+    if(val%2!=0){
+       median=a[(val/2)-1];
+       return median;
+    }
+    if(val%2==0){
+        median=(a[(val/2)-1]+a[(val/2)])/2;
+        return median;
     }
 }
 /**
@@ -73,7 +102,14 @@ int med(unsigned char *a,int val){
  * @return <returns the variable maximum,which holds the maximum number in the array>
  */
 int max(unsigned char *a,int val){
-  
+    int maximum=0;
+    for(int i=0;i<val;i++){
+        if(maximum<a[i]){
+            maximum=a[i];
+        }
+    }
+   
+    return maximum;
 }
 /**
  * @brief <function that finds the minimum>
@@ -86,7 +122,13 @@ int max(unsigned char *a,int val){
  * @return <returns the variable minimum,which holds the minimum number in the array>
  */
 int min(unsigned char *a,int val,int maximum){
-    
+    int minimum=maximum;
+    for(int i=0;i<val;i++){
+        if(minimum>a[i]){
+            minimum=a[i];
+        }
+    }
+    return minimum;
 }
 /**
  * @brief <function that sorts the input array and prints it>
@@ -99,6 +141,24 @@ int min(unsigned char *a,int val,int maximum){
  * @return <returns the value 0>
  */
 int sort(unsigned char *a,int val){
-  
+    int swap=0;
+    for (int c = 0 ; c < val - 1; c++)
+  {
+    for (int d = 0 ; d < val - c - 1; d++)
+    {
+      if (a[d] > a[d+1]) /* For decreasing order use < */
+      {
+        swap       = a[d];
+        a[d]   = a[d+1];
+        a[d+1] = swap;
+      }
+    }
+  }
+  for(int i=0;i<val;i++){ 
+        long int b= a[i];
+        printf("%d\n",b);
+    }
+    printf("\n");
+	return(0);
 }
 #endif /* __STATS_H__ */
